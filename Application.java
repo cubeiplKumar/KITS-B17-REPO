f71e264f9d3751abc921b71fdcf8a85fac0b160e
cubeiplkumar <emailanilkumarm@gmail.com>
2025-01-22 12:00:10 +0530

    feature201 changes in Application.java file

--- Application.java
+++ Application.java
@@ -20,5 +20,23 @@ class Main {
     int result = obj.addNumbers(num1, num2);
     System.out.println("Sum is: " + result);
   }
+  #feature201 changes by ethan for the module reports on oct2024
+  public static int square(int num) {
+
+    // return statement
+    return num * num;
+  }
+
+  public static void main(String[] args) {
+    int result;
+
+    // call the method
+    // store returned value to result
+    result = square(10);
+
+    System.out.println("Squared value of 10 is: " + result);
+  }
+}
+#end of feature201
 }
 
